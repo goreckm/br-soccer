686445d2188b8e7910492c2e5061151a8a4287ef
Michal Gorecki <goreckm@gmail.com>
2012-05-31 19:21:47 -0400

    Copy of original tournament page up.

---
+++ BR.Soccer/Controllers/TournamentController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BR.Soccer.Models;
+
+namespace BR.Soccer.Controllers
+{
+    public class TournamentController : Controller
+    {
+        //
+        // GET: /Tournament/
+
+        public ActionResult Index()
+        {
+            var tournamentList = new List<Tournament>()
+            {
+                new Tournament { Id = 1, Name = "FIFA 4 Star Challenge" }
+            };
+
+            return View(tournamentList);
+        }
+
+        //
+        // GET: /Tournament/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var tournament = new Tournament { Id = 1, Name = "FIFA 4 Star Challenge" };
+
+            return View(tournament);
+        }
+
+        //
+        // GET: /Tournament/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Tournament/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Tournament/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Tournament/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Tournament/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Tournament/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BR.Soccer/Models/Tournament.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BR.Soccer.Models
+{
+    public class Tournament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
